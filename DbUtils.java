6bd3adb361fe9ff67539d17f336fbe710fdd66fe
ajayjamliya <ajayjamliya76@gmail.com>
2022-12-02 18:02:47 +0530

    Reverted done Revert "comitted"
    This reverts commit b729851fa68461aba11d3fa550ff0dc90b1e38ab.

--- DbUtils.java
+++ DbUtils.java
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
 This us updated line 
-=======
-Hello i am updating this file becoz i wan tot use that command and commanf is git commit -a -m "updated"
->>>>>>> parent of 19eb514... Revert "updated"
 This is the Database file... contens some DB logic
